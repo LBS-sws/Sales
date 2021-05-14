8abd7dddfedadd2477c96472503e264a5048623f
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-05-14 17:41:09 +0800

    积分兑换判断

--- sales/protected/controllers/RedeemController.php
+++ sales/protected/controllers/RedeemController.php
@@ -24,7 +24,7 @@ class RedeemController extends Controller
     {
         return array(
             array('allow',
-                'actions'=>array('new','edit','delete','save','downs','test','apply','settings'),
+                'actions'=>array('new','edit','delete','save','downs','test','apply'),
                 'expression'=>array('RedeemController','allowReadWrite'),
             ),
             array('allow',
@@ -80,28 +80,7 @@ class RedeemController extends Controller
         //var_dump($model);die();
         $this->render('index',array('model'=>$model));
     }
-    public function actionSettings($pageNum=0)
-    {
-        $model = new RedeemGifts();
-        if (isset($_POST['GiftList'])) {
-            $model->attributes = $_POST['GiftList'];
-        } else {
-            $session = Yii::app()->session;
-            if (isset($session['gift_op01']) && !empty($session['gift_op01'])) {
-                $criteria = $session['gift_op01'];
-                $model->setCriteria($criteria);
-            }
-        }
-        if (isset($_POST['RedeemGifts'])){
-            $model->attributes = $_POST['RedeemGifts'];
-        }
-        $model->determinePageNum($pageNum);
-        $model->retrieveDataByPage($model->pageNum);
-        /*        $cutIntegral = IntegralCutView::getNowIntegral();
-                $this->render('index',array('model'=>$model,'cutIntegral'=>$cutIntegral));*/
-        //var_dump($model);die();
-        $this->render('settings',array('model'=>$model));
-    }
+
     public function actionNew()
     {
         $model = new RedeemGifts('new');
@@ -111,10 +90,26 @@ class RedeemController extends Controller
         $model = new RedeemgiftApply("apply");
         if($model->validateNowUser(true)){
             if (isset($_POST['GiftApply'])) {
+
                 $model->attributes = $_POST['GiftApply'];
                 if ($model->validate()) {
-                    $model->saveData();
-                    Dialog::message(Yii::t('dialog','Information'), Yii::t('dialog','Save Done'));
+                    //礼物信息
+                    $city = Yii::app()->user->city();
+                    $sqlg = "SELECT * FROM sal_redeem_gifts WHERE gift_name='".$_POST['GiftApply']['gift_name']."' and bonus_point=".$_POST['GiftApply']['bonus_point']." and city='".$city."'";
+                    $gift = Yii::app()->db->createCommand($sqlg)->queryRow();
+                    //判断积分和库存
+                    $listArrIntegral = RedeemGifts::getNowIntegral();
+                    $sy_score = $listArrIntegral['cut']-$_POST['GiftApply']['bonus_point']*$_POST['GiftApply']['apply_num'];
+                    $sy_inventory =$gift['inventory']-$_POST['GiftApply']['apply_num'];
+                    if ($sy_score < 0){
+                        Dialog::message(Yii::t('dialog','Information'), Yii::t('dialog','Score No'));
+                    }else if($sy_inventory<0){
+                        Dialog::message(Yii::t('dialog','Information'), Yii::t('dialog','Inventory No'));
+                    }else{
+                        $model->saveData();
+                        Dialog::message(Yii::t('dialog','Information'), Yii::t('dialog','Save Done'));
+                    }
+
                     $this->redirect(Yii::app()->createUrl('redeem/index'));
                 } else {
                     $message = CHtml::errorSummary($model);
--- sales/protected/messages/zh_cn/dialog.php
+++ sales/protected/messages/zh_cn/dialog.php
@@ -85,5 +85,7 @@ return array(
 ',
     'RankScoreForm'=>'销售段位总分榜',
     'You can only select one of them separately to view season data and annual data'=>'查看赛季数据和查看年度数据只能单独选择其中一项',
+    'Score No'=>'积分不足',
+    'Inventory No'=>'库存不足',
 );
 ?>
