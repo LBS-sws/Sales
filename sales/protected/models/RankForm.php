5897237da022a9ccd983f937112aacaaad4dfff5
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-05-19 10:04:52 +0800

    段位分数变动后修改

--- sales/protected/models/RankForm.php
+++ sales/protected/models/RankForm.php
@@ -85,10 +85,6 @@ class RankForm extends CFormModel
             $rows['last_score'] = 0;
         }
         $this->last_score = $rows['last_score'];
-        var_dump($rows);
-        var_dump($rows['last_score']);
-        var_dump($this->last_score);
-        die();
         //赛季
         $this->season = $this->numToWord($rows['season']);
         //销售人员名称
@@ -497,7 +493,7 @@ class RankForm extends CFormModel
         }
         //当前赛季总分（继承后）
         $this->now_score = round($this->all_score + $this->last_score, 2);
-       // $this->now_score = 30001;
+       $this->now_score = 60001;
         //上赛季段位
         //上赛季段位
         $sql_rank_name = "select * from sal_level where start_fraction <='" . $this->now_score . "' and end_fraction >='" . $this->now_score . "'";
@@ -520,7 +516,7 @@ class RankForm extends CFormModel
         }
 
 
-        $sql1 = "update sal_rank set all_score='" . $this->all_score . "',last_score='" . $this->last_score . "',now_score='" . $this->now_score . "',initial_score='" . $this->initial_score . "' where id='" . $index . "'";
+        $sql1 = "update sal_rank set all_score='" . $this->all_score . "',last_score='" . $rows['now_score'] . "',now_score='" . $this->now_score . "',initial_score='" . $this->initial_score . "' where id='" . $index . "'";
         $command = Yii::app()->db->createCommand($sql1)->execute();
         return true;
     }
