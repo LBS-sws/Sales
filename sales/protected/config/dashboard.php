44f217c36f227742910493aa9fd32b45d8d33d51
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-06-08 09:58:49 +0800

    销售首页排行榜位置调整

--- sales/protected/config/dashboard.php
+++ sales/protected/config/dashboard.php
@@ -3,7 +3,7 @@
 return (array(
     array('view'=>'ranklist', 'row'=>1, 'width'=>'col-md-6', 'access'=>'HD01'),
     array('view'=>'salepeople', 'row'=>0, 'width'=>'col-md-6', 'access'=>''),
-	array('view'=>'salelist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
+    array('view'=>'salelist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
     array('view'=>'salelists', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
     array('view'=>'notification', 'row'=>0, 'width'=>'col-md-4', 'access'=>'CN04'),
 ));
