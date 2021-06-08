a3394bfab9027fddaec178ae31a1d3c0d33e5c6d
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-06-08 10:34:50 +0800

    销售首页排行榜位置调整测试

--- sales/protected/config/dashboard.php
+++ sales/protected/config/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 
 return (array(
+
     array('view'=>'ranklist', 'row'=>1, 'width'=>'col-md-6', 'access'=>'HD01'),
     array('view'=>'salepeople', 'row'=>0, 'width'=>'col-md-6', 'access'=>''),
     array('view'=>'salelist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
