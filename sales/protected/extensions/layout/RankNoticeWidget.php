986a5978a8f64ae389ae873777f79559b8364ec9
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-06-11 15:30:30 +0800

    段位前五弹框样式-测试版测试02

--- sales/protected/extensions/layout/RankNoticeWidget.php
+++ sales/protected/extensions/layout/RankNoticeWidget.php
@@ -112,7 +112,7 @@ EOF;
 		$js = <<<EOF
 $('#modal-ranknotice').modal('show');
 $('#modal-ranknotice').on("hidden.bs.modal", function() {
-	
+	$('#modal-default').modal('show');
 });
 EOF;
 		Yii::app()->clientScript->registerScript('ranknotice',$js,CClientScript::POS_READY);
