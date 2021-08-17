b5175a34fbe9386f7bbe88289498919166007b84
shenchao <shenchao@lbsgroup.com.cn>
2021-08-17 16:17:43 +0800

    測驗系統的彈窗提示

--- sales/protected/messages/en/block.php
+++ sales/protected/messages/en/block.php
@@ -1,5 +1,7 @@
 <?php
 return array(
-//	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。'
+    'validateExamination'=>'The average score of customer service last month was less than 75. Please log in the test system to do the test<br/>Requirement: 85% correct answer rate, otherwise you need to continue the test',
+    'validateNewStaff'=>'Welcome to Join Swisher to ensure service quality<br>Please complete the first training test within 3 months from the date of commencement<br/>Requirement: 85% correct answer rate, otherwise you need to continue the test',
+    'EveryYearForExamination'=>'Dear colleagues, in order to consolidate the technical service knowledge and service quality<br/> please complete the annual training test by the end of next month',
 );
 ?>
