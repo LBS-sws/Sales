4e8fe672ffb4968111942031d0b8f4bf9370cd81
lbs-percylee <percylee@lbsgroup.com.hk>
2023-04-06 15:08:07 +0800

    保留销售部离职同事的销售五部曲记录

--- sales/protected/commands/FivestepCommand.php
+++ sales/protected/commands/FivestepCommand.php
@@ -6,17 +6,19 @@ class FivestepCommand extends CConsoleCommand
         $suffix = Yii::app()->params['envSuffix'];
         $sql="select a.id ,a.filename from sales$suffix.sal_fivestep a 
 	          left outer join security$suffix.sec_user b on a.username=b.username
-	          where b.status='I'";
+	          where b.status='I' and b.lud>='".date('Y-m-d',strtotime('-1 day'))."'";
         $arr=Yii::app()->db->createCommand($sql)->queryAll();
         if(!empty($arr)){
             foreach ($arr as $a){
                 if(file_exists($a['filename'])){
                     unlink($a['filename']);
                 }
-                $sql1="delete from sales$suffix.sal_fivestep where id = '".$a['id']."'";
+/*
+				$sql1="delete from sales$suffix.sal_fivestep where id = '".$a['id']."'";
                 $command=Yii::app()->db->createCommand($sql1)->execute();
                 $sql2="delete from sales$suffix.sal_fivestep_info where five_id = '".$a['id']."'";
                 $commands=Yii::app()->db->createCommand($sql2)->execute();
+*/
             }
         }
     }
