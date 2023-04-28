4f7c8411a7720629ea59297a3b8d42bfbf99b0ce
shenchao <shenchao@lbsgroup.com.cn>
2023-04-28 13:00:45 +0800

    Invoice表未同步，無法使用

--- sales/protected/models/ComparisonForm.php
+++ sales/protected/models/ComparisonForm.php
@@ -108,7 +108,8 @@ class ComparisonForm extends CFormModel
             }
         }
 
-        $this->insertUData($this->start_date,$this->end_date,$lastStartDate,$lastEndDate,$data);
+        $this->insertUData($this->start_date,$this->end_date,$data);
+        $this->insertUData($lastStartDate,$lastEndDate,$data);
         $this->insertUActualMoney($this->start_date,$this->end_date,$data);//服务生意额
         $this->data = $data;
         return true;
@@ -137,22 +138,14 @@ class ComparisonForm extends CFormModel
         return $list;
     }
 
-    private function insertUData($startDate,$endDate,$lastStartDate,$lastEndDate,&$data){
-        $list = array();
-        $suffix = Yii::app()->params['envSuffix'];
-        $rows = Yii::app()->db->createCommand()
-            ->select("x.InvoiceDate,x.InvoiceAmount,b.Text AS City")
-            ->from("service{$suffix}.Invoice x")
-            ->leftJoin("service{$suffix}.OfficeCity a","x.City = a.City")
-            ->leftJoin("service{$suffix}.Enums b","a.Office = b.EnumID AND b.EnumType = 8")
-            ->where("x.status>0 and ((x.InvoiceDate BETWEEN '{$startDate}' AND '{$endDate}') or (x.InvoiceDate BETWEEN '{$lastStartDate}' AND '{$lastEndDate}'))AND SUBSTRING(x.InvoiceNumber, 3, 3) = 'INV'")
-            ->order("x.InvoiceDate,x.CustomerID,x.InvoiceNumber")
-            ->queryAll();
-        if($rows){
-            foreach ($rows as $row){
-                $year = intval($row["InvoiceDate"]);//服务的年份
-                $city = $row["City"];
-                $money = is_numeric($row["InvoiceAmount"])?floatval($row["InvoiceAmount"]):0;
+    private function insertUData($startDate,$endDate,&$data){
+        $year = intval($startDate);//服务的年份
+        $json = Invoice::getInvData($startDate,$endDate);
+        if($json["message"]==="Success"){
+            $jsonData = $json["data"];
+            foreach ($jsonData as $row){
+                $city = $row["city"];
+                $money = is_numeric($row["invoice_amt"])?floatval($row["invoice_amt"]):0;
                 if(key_exists($city,$data)){
                     if($year==$this->comparison_year){
                         $uStr = "u_sum";
