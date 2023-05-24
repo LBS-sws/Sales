bcc02ee41da77bcbb3a4b9e5aceacafc20fb078f
shenchao <shenchao@lbsgroup.com.cn>
2023-05-24 13:40:30 +0800

    優化銷售匯總的銷售選擇（5）

--- sales/protected/models/ReportVisitForm.php
+++ sales/protected/models/ReportVisitForm.php
@@ -91,9 +91,9 @@ class ReportVisitForm extends CReportForm
             ->leftJoin("hr{$suffix}.hr_binding d","d.user_id=f.username")
             ->leftJoin("hr{$suffix}.hr_employee a","d.employee_id=a.id")
             ->where("f.system_id='sal' and f.a_read_write like '%HK01%' and (
-                (a.staff_status!=1 and date_format(a.entry_time,'%Y/%m/%d')<='{$endDate}')
+                (a.staff_status = 0 and date_format(a.entry_time,'%Y/%m/%d')<='{$endDate}')
                 or
-                (a.staff_status=-1 AND date_format(a.leave_time,'%Y/%m/%d') between '{$startDate}' and '{$endDate}')
+                (a.staff_status=-1 and date_format(a.leave_time,'%Y/%m/%d')>='{$startDate}' and date_format(a.entry_time,'%Y/%m/%d')<='{$endDate}')
              ) AND a.city in ({$city_allow})"
             )->order("a.id desc")->queryAll();
         if($rows){
