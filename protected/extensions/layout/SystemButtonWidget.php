2039e50a30b19130f1fc6df8c40b86c797e4ccad
xiangsong <xiangsong.cd@lbsgroup.com.cn>
2025-12-28 20:48:32 +0800

    Update SystemButtonWidget.php

--- protected/extensions/layout/SystemButtonWidget.php
+++ protected/extensions/layout/SystemButtonWidget.php
@@ -1,8 +1,8 @@
 <?php
 class SystemButtonWidget extends CWidget {
-    public function run() {
+	public function run() {
 // System Change Button
-        $js = <<<EOF
+		$js = <<<EOF
 $(function () {
   $('[data-toggle=\"tooltip\"]').tooltip()
 });
@@ -11,20 +11,25 @@ $('#btnSysChange').on('click',function() {
 	$('#syschangedialog').modal('show');
 });
 EOF;
-        $incl_js = false;
-        foreach (General::systemMapping() as $id=>$value) {
-            if (Yii::app()->user->validSystem($id)) {
-                $oid = 'btnSys'.$id;
-                $url = $value['webroot'];
-                if (!isset($value['script'])) {
-                    $temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");window.location="'.$url.'";});';
+		$incl_js = false;
+		foreach (General::systemMapping() as $id=>$value) {
+			if (Yii::app()->user->validSystem($id)) {
+				$oid = 'btnSys'.$id;
+				$url = $value['webroot'];
+				if (!isset($value['script'])) {
+					$temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");window.location="'.$url.'";});';
                 }elseif($id=='nu'){
                     $url = $value['webroot'].$value['param'];
                     $incl_js = true;
                     $homeurl = $value['webroot'];
+                    //查询staff_employ表
+                    $suffix = Yii::app()->params['envSuffix'];
+                    $sql ='select `e`.code from `hr'.$suffix.'`.`hr_binding` `h` join `hr'.$suffix.'`.`hr_employee` `e` on `h`.employee_id=`e`.id where `h`.user_id="'.Yii::app()->user->id.'"';
+                    $rows = Yii::app()->db->createCommand($sql)->queryRow();
                     //构建数据
                     $data = array(
                         'user_id'=>Yii::app()->user->id,
+                        'code'=> !empty($rows['code']) ? $rows['code'] : false,
                         'system'=>Yii::app()->session['system'],
                         'city'=>Yii::app()->session['city'],
                         'disp_name'=>Yii::app()->session['disp_name'],
@@ -37,23 +42,23 @@ EOF;
                     $jsonString = ($key.$String);
                     $encryptedString = base64_encode($jsonString);
                     //拼接
-                    $temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");'.$value['script'].'("'.$id.'","'.$url.'","'.$homeurl.'","'.$encryptedString.'","'.$data['user_id'].'");});';
+                    $temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");'.$value['script'].'("'.$id.'","'.$url.'","'.$homeurl.'","'.$encryptedString.'","'.$data['user_id'].'","'.$data['code'].'");});';
                 } else {
-                    $func_name = $value['script'];
-                    $lang = Yii::app()->language;
-                    $homeurl = Yii::app()->createUrl("");
-                    $incl_js = true;
-                    $temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");'.$func_name.'("'.$id.'","'.$url.'","'.$homeurl.'");});';
-                }
-                $js .= $temp;
-            }
-        }
-
-        if ($incl_js) {
-            $sfile = Yii::app()->baseUrl.'/js/systemlink.js';
-            Yii::app()->clientScript->registerScriptFile($sfile,CClientScript::POS_HEAD);
-        }
-        Yii::app()->clientScript->registerScript('systemchange',$js,CClientScript::POS_READY);
+					$func_name = $value['script'];
+					$lang = Yii::app()->language;
+					$homeurl = Yii::app()->createUrl("");
+					$incl_js = true;
+					$temp = '$("#'.$oid.'").on("click",function(){$("#syschangedialog").modal("hide");'.$func_name.'("'.$id.'","'.$url.'","'.$homeurl.'");});';
+				}
+				$js .= $temp;
+			}
+		}
+	
+		if ($incl_js) {
+			$sfile = Yii::app()->baseUrl.'/js/systemlink.js';
+			Yii::app()->clientScript->registerScriptFile($sfile,CClientScript::POS_HEAD);
+		}
+		Yii::app()->clientScript->registerScript('systemchange',$js,CClientScript::POS_READY);
 // End - System Chnage Button
-    }
+	}
 }
